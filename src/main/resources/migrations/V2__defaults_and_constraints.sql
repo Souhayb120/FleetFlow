c2908ceb019457a719af93a743cae46a7d9192b6
Souhayb120 <souhaybhadi06@gmail.com>
2026-04-06 12:27:09 +0100

    part1 sql

---
+++ src/main/resources/migrations/V2__defaults_and_constraints.sql
@@ -0,0 +1,33 @@
+
+ALTER TABLE livraison
+ADD CONSTRAINT fk_client
+        FOREIGN KEY (client_id) REFERENCES client(id),
+
+ADD CONSTRAINT fk_chauffeur
+FOREIGN KEY (chauffeur_id) REFERENCES chauffeur(id),
+
+ADD CONSTRAINT fk_vehicule
+FOREIGN KEY (vehicule_id) REFERENCES vehicule(id);
+
+
+ALTER TABLE vehicle
+ADD CONSTRAINT fk_chauffeur
+FOREIGN KEY (chauffeur_id) REFERENCES chauffeur(id);
+
+
+ALTER TABLE chauffeur
+    ALTER COLUMN isDisponible SET DEFAULT true;
+
+ALTER TABLE livraison
+    ALTER COLUMN statut SET DEFAULT 'ENATTENTE';
+
+ALTER TABLE vehicle
+    ALTER COLUMN disponible SET DEFAULT true;
+
+
+
+ALTER TABLE vehicle
+    ADD CONSTRAINT chk_capacite CHECK (capacite > 0);
+
+ALTER TABLE vehicle
+    ADD CONSTRAINT uq_matricule UNIQUE (matricule);
